c538f104a65c675f7c8d4422ebde4cf41d3c5335
MichaelLah <michael@michaellah.com>
2021-01-09 21:04:51 -0500

    Add old site

---
+++ precache-manifest.32b4c34709286daafe2591bc1bf1fe2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71e9428553ee396edc7b33877ee6c5f4",
+    "url": "/static/media/beam_logo.71e94285.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cd696a9c68443bb14d0b",
+    "url": "/static/js/main.39842bc9.chunk.js"
+  },
+  {
+    "revision": "924c3ec0902a58759e1f",
+    "url": "/static/js/2.bad41068.chunk.js"
+  },
+  {
+    "revision": "cd696a9c68443bb14d0b",
+    "url": "/static/css/main.34a106f0.chunk.css"
+  },
+  {
+    "revision": "7c0c616f6357e5f29b3f7e90bd89ca3b",
+    "url": "/index.html"
+  }
+];
